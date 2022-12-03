5da2431fa0d622808bee70d2b28956dffbe78c4c
Mohammad Shireen <94127561+303-MdShireen@users.noreply.github.com>
2022-12-03 17:22:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post("/todos", async function (request, response) {
 app.put("/todos/:id/markAsCompleted", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
   try {
-    const updatedTodo = await Todo.markAsCompleted();
+    const updatedTodo = await todo.markAsCompleted();
     return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
