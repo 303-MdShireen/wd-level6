60924c8a2f2630912be7eedf050e3caa4d5ab29b
Mohammad Shireen <shireenmd402@gmail.com>
2023-01-08 15:53:59 +0530

    level-10

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const app = require("./app.js");
 const express = require("express");
 const path = require("path");
@@ -7,14 +6,6 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(process.env.PORT || 2100, () => {
-  console.log("Started express server");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Started express server at port 3000");
 });
-=======
-const app = require("./app");
-
-const PORT = process.env.PORT || 1600;
-app.listen(PORT, () => {
-  console.log(`Started express sever on port ${PORT}`);
-});
->>>>>>> c77a4870977e3e3b79493903b44f692d712168a9
