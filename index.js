0b4c8d9dc8db7b2c9adedbdb1d286145cef8045e
Mohammad Shireen <shireenmd402@gmail.com>
2023-01-16 17:22:12 +0530

    level-10

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(process.env.PORT || 4600, () => {
+app.listen(process.env.PORT || 4640, () => {
   console.log("Started express server");
 });
